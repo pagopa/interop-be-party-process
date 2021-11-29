fede7cd3e029c4d88646a5e98073995bb3fb59fe
Stefano Perazzolo <stefano.perazzolo@gmail.com>
2021-11-29 17:25:02 +0100

    fix: RelationshipOps use Class
    Co-authored-by: Stefano Perazzolo <stefano.perazzolo@pagopa.it>

--- src/main/scala/it/pagopa/pdnd/interop/uservice/partyprocess/api/impl/package.scala
+++ src/main/scala/it/pagopa/pdnd/interop/uservice/partyprocess/api/impl/package.scala
@@ -10,17 +10,17 @@ package object impl extends DefaultJsonProtocol {
   implicit val tokenChecksumFormat: RootJsonFormat[TokenChecksum]           = jsonFormat1(TokenChecksum)
   implicit val problemFormat: RootJsonFormat[Problem]                       = jsonFormat3(Problem)
   implicit val userFormat: RootJsonFormat[User]                             = jsonFormat7(User)
-  implicit val onBoardingRequestFormat: RootJsonFormat[OnboardingRequest]   = jsonFormat2(OnboardingRequest)
-  implicit val onBoardingResponseFormat: RootJsonFormat[OnboardingResponse] = jsonFormat2(OnboardingResponse)
+  implicit val onboardingRequestFormat: RootJsonFormat[OnboardingRequest]   = jsonFormat2(OnboardingRequest)
+  implicit val onboardingResponseFormat: RootJsonFormat[OnboardingResponse] = jsonFormat2(OnboardingResponse)
   implicit val personInfoFormat: RootJsonFormat[PersonInfo]                 = jsonFormat3(PersonInfo)
   implicit val attributeDataFormat: RootJsonFormat[Attribute]               = jsonFormat3(Attribute.apply)
   implicit val productInfoDataFormat: RootJsonFormat[ProductInfo]           = jsonFormat3(ProductInfo)
   implicit val onboardingDataFormat: RootJsonFormat[OnboardingData]         = jsonFormat8(OnboardingData)
-  implicit val onBoardingInfoFormat: RootJsonFormat[OnboardingInfo]         = jsonFormat2(OnboardingInfo)
+  implicit val onboardingInfoFormat: RootJsonFormat[OnboardingInfo]         = jsonFormat2(OnboardingInfo)
   implicit val relationshipInfoFormat: RootJsonFormat[RelationshipInfo]     = jsonFormat5(RelationshipInfo)
   implicit val productsFormat: RootJsonFormat[Products]                     = jsonFormat1(Products)
 
-  implicit case class RelationshipOps(relationship: Relationship) extends AnyVal {
+  implicit class RelationshipOps(val relationship: Relationship) extends AnyVal {
     def verifyProducts(products: List[String]): Boolean = {
       products.isEmpty || products.contains(relationship.product.id)
     }
