49bc33ade2270d521955163538fab4af4312995e
galales <25105748+galales@users.noreply.github.com>
2022-03-11 13:04:29 +0100

    PIN-1224: Fix deploy and update deps

--- project/Versions.scala
+++ project/Versions.scala
@@ -18,8 +18,8 @@ object Versions {
 }
 
 object PagopaVersions {
-  lazy val commonsVersion                = "0.1.1-SNAPSHOT"
-  lazy val partyManagementVersion        = "0.1.5-SNAPSHOT"
-  lazy val partyProxyVersion             = "0.1.3-SNAPSHOT"
-  lazy val userRegistryManagementVersion = "0.1.1-SNAPSHOT"
+  lazy val commonsVersion                = "0.1.1"
+  lazy val partyManagementVersion        = "0.1.7"
+  lazy val partyProxyVersion             = "0.1.3"
+  lazy val userRegistryManagementVersion = "0.1.1"
 }
