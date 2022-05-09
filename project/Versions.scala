4728a4499eb1b2034a1ca68ad0683f54792dd92e
Stefano Perazzolo <stefano.perazzolo@gmail.com>
2022-05-09 14:22:50 +0200

    revert PagopaVersions

--- project/Versions.scala
+++ project/Versions.scala
@@ -16,8 +16,8 @@ object Versions {
 }
 
 object PagopaVersions {
-  lazy val commonsVersion                = "0.1.3"
-  lazy val partyManagementVersion        = "0.1.13"
-  lazy val partyProxyVersion             = "0.1.9"
-  lazy val userRegistryManagementVersion = "0.1.2"
+  lazy val commonsVersion                = "0.1.x-SNAPSHOT"
+  lazy val partyManagementVersion        = "0.1.x-SNAPSHOT"
+  lazy val partyProxyVersion             = "0.1.x-SNAPSHOT"
+  lazy val userRegistryManagementVersion = "0.1.x-SNAPSHOT"
 }
