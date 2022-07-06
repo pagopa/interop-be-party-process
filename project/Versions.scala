861d5093aeeaba77392d547daabcfc1f373bc375
Antonio Gelameris <toniogela89@gmail.com>
2022-07-06 14:03:22 +0200

    Combined Scala Steward PR (#149)
    * manual update
    
    * Reverting json4s

--- project/Versions.scala
+++ project/Versions.scala
@@ -3,16 +3,16 @@ object Versions {
   lazy val akkaHttpVersion       = "10.2.6"
   lazy val akkaManagementVersion = "1.1.1"
   lazy val akkaVersion           = "2.6.17"
-  lazy val catsVersion           = "2.7.0"
+  lazy val catsVersion           = "2.8.0"
   lazy val courierVersion        = "3.0.1"
-  lazy val dssVersion            = "5.10"
+  lazy val dssVersion            = "5.10.1"
   lazy val jacksonVersion        = "2.11.4"
   lazy val json4sVersion         = "4.0.4"
-  lazy val kamonVersion          = "2.5.1"
+  lazy val kamonVersion          = "2.5.5"
   lazy val logbackVersion        = "1.2.11"
   lazy val mustacheVersion       = "0.9.10"
   lazy val scalaMockVersion      = "5.2.0"
-  lazy val scalatestVersion      = "3.2.11"
+  lazy val scalatestVersion      = "3.2.12"
 }
 
 object PagopaVersions {
