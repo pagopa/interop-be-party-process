93dcf22cfa0c5f125f81fec2a5c08efae6486db4
Stefano Perazzolo <stefano.perazzolo@gmail.com>
2022-07-26 18:31:05 +0200

    add comments

--- src/test/scala/it/pagopa/interop/partyprocess/service/impl/SignatureSpec.scala
+++ src/test/scala/it/pagopa/interop/partyprocess/service/impl/SignatureSpec.scala
@@ -13,13 +13,28 @@ import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration._
 import scala.concurrent.{Await, Future}
 
+/**
+ * Test instructions
+ * This test is useful to verify the document signature validation.
+ * These are the steps to run the test:
+ *  - comment @Ignore annotation
+ *  - comment import org.scalatest.Ignore
+ *  - set unsignedDocumentPath
+ *  - set signedDocumentPath
+ *  - For each signature present, add a UserRegistryUser entry to the legals parameter 
+ *    of the SignatureValidationServiceImpl.verifyManagerTaxCode function.
+ *  - For each UserRegistryUser added, set the taxCode field using the tax code associated with the signature.
+ *  - run command 'testOnly it.pagopa.interop.partyprocess.service.impl.SignatureSpec' from sbt repl
+ */
 @Ignore
 class SignatureSpec extends AnyWordSpecLike with Matchers with Dependencies {
 
   "signature must work" in {
 
+    // Set the path of the unsigned document
     val unsignedDocumentPath: Path = Paths.get("unsigned doc path")
 
+    // Set the path of the signed document
     val signedDocumentPath: Path = Paths.get("signed doc path")
 
     val signedDocumentBytes: Array[Byte] = Files.readAllBytes(signedDocumentPath)
@@ -40,10 +55,9 @@ class SignatureSpec extends AnyWordSpecLike with Matchers with Dependencies {
           SignatureValidationServiceImpl.verifyDigest(validator, unsignedDigest),
           SignatureValidationServiceImpl.verifyManagerTaxCode( // To pass this test taxCode must be set with real ones
             reports,
-            Seq(
-              UserRegistryUser(id = UUID.randomUUID(), name = "", surname = "", taxCode = "tax code 1"),
-              UserRegistryUser(id = UUID.randomUUID(), name = "", surname = "", taxCode = "tax code 2")
-            )
+            // This sequence must contain a UserRegistryUser entry for each signer contained in the document signature.
+            // For each signature present, the taxCode field must be set with the real tax code associated with the signature.
+            Seq(UserRegistryUser(id = UUID.randomUUID(), name = "", surname = "", taxCode = "tax code 1"))
           )
         )
     } yield "OK"
