c3066533b1b74bd3cc4aef81eae0c85ff8135470
andrea-putzu <andrea.putzu@pagopa.it>
2022-09-30 10:45:13 +0200

    fix: Refactor match -> if/else

--- src/main/scala/it/pagopa/interop/partyprocess/api/impl/PublicApiServiceImpl.scala
+++ src/main/scala/it/pagopa/interop/partyprocess/api/impl/PublicApiServiceImpl.scala
@@ -72,10 +72,10 @@ class PublicApiServiceImpl(
       institutionId <- Future.traverse(legalsRelationships)(legalUser =>
         partyManagementService.getInstitutionId(legalUser.relationshipId)
       )
-      institutionEmail      = if (ApplicationConfiguration.sendEmailToInstitution)
-      institutionId.headOption.map(_.digitalAddress)
+      institutionEmail =
+        if (ApplicationConfiguration.sendEmailToInstitution) institutionId.headOption.map(_.digitalAddress)
         else Some(ApplicationConfiguration.institutionAlternativeEmail)
-      
+
       institutionInternalId = institutionId.headOption.map(_.to.toString)
       legalUserWithEmails   = legalUsers.filter(_.email.get(institutionInternalId.getOrElse("")).nonEmpty)
       legalEmails           = legalUserWithEmails.map(u => u.email.get(institutionInternalId.getOrElse("")))
